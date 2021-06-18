2b972b0e6f73fa2e7749f5698059fe079ac9e960
503369121 <78159229+503369121@users.noreply.github.com>
2021-06-18 21:34:20 +0800

    Update omni_cannong.mk

--- omni_cannong.mk
+++ omni_cannong.mk
@@ -23,16 +23,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit from cannong device
-$(call inherit-product, device/xiaomi/cannong/device.mk)
+# Inherit from cannon device
+$(call inherit-product, device/xiaomi/cannon/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 #$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := cannong
-PRODUCT_NAME := omni_cannong
+PRODUCT_DEVICE := cannon
+PRODUCT_NAME := omni_canno
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi
 PRODUCT_MANUFACTURER := xiaomi
