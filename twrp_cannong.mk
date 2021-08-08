102e1da989a951a7c04aac71c154d9adfd6f27c3
Uzeda <40251034+gsuzeda@users.noreply.github.com>
2021-08-08 19:17:59 -0300

    updated to android 11

---
+++ twrp_cannong.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit from cannong device
+$(call inherit-product, device/xiaomi/cannong/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := cannong
+PRODUCT_NAME := twrp_cannong
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_RELEASE_NAME := Redmi Redmi
+
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
