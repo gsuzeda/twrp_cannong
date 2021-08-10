1fda6c3bf44126c3bc55c1676899bf37e9264f17
Hemanth <hemanthjabalpuri@gmail.com>
2021-08-10 19:16:27 +0530

    Big Cleanup (#3)
    * Cleanup 1
    
    * Fix mistake in before cleanup
    
    * Fix mistake in before cleanup
    
    * Cleanup 2
    
    * Add missing lib
    It is need for gatekeeper.mt6853.so

--- device.mk
+++ device.mk
@@ -18,10 +18,11 @@
 
 LOCAL_PATH := device/xiaomi/cannong
 
-# Fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock
