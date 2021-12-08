d8487bf7ad189218da871bf92bca621061b88593
Uzeda <gabrielsuzeda@gmail.com>
2021-12-07 23:59:05 -0300

    Magisk updated

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -61,7 +61,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_STATUS_INDENT_LEFT="150"
 	export OF_STATUS_H="98"
 	export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	export OF_AB_DEVICE=1
 	export USE_CCACHE=1
 	
 	# OTA
